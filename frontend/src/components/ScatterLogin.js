32dc58f55bb2cb0a05e17b65f060570afe52f2f8
treasersimplifies <31153014+treasersimplifies@users.noreply.github.com>
2019-03-11 18:39:54 +0800

    fix testnet login bug.

--- frontend/src/components/ScatterLogin.js
+++ frontend/src/components/ScatterLogin.js
@@ -3,10 +3,10 @@ import React, { Component } from "react";
 // import { post } from "../utils/request";
 // import url from "../utils/url";
 import "./ScatterLogin.css";
-// import ScatterJS from 'scatterjs-core';
-// import ScatterEOS from 'scatterjs-plugin-eosjs';
-import ScatterJS from 'zjubca-scatterjs-core'
-import ScatterEOS from 'zjubca-scatterjs-plugin-eosjs'
+import ScatterJS from 'scatterjs-core';
+import ScatterEOS from 'scatterjs-plugin-eosjs';
+// import ScatterJS from 'zjubca-scatterjs-core'
+// import ScatterEOS from 'zjubca-scatterjss-plugin-eosjs'
 import Eos from 'eosjs';
 
 const mode = "test";
@@ -28,8 +28,8 @@ class ScatterLogin extends Component {
     }
     currentAccount = null;
     connected = false;
-
     CONTRACT = 'zjubcabounty';//????
+
     network = {//NETWORK
         blockchain: 'eos',//
         protocol: 'http',//https
@@ -37,9 +37,15 @@ class ScatterLogin extends Component {
         port: 8888,//443
         chainId: 'cf057bbfb72640471fd910bcb67639c22df9f92470936cddc1ade0e2f2e7dc4f'
         // aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906
-        //
-        // chainId ???
-    };
+    }
+
+    // network = {
+    //     blockchain: 'eos',
+    //     protocol: 'https',
+    //     host: 'nodes.get-scatter.com',
+    //     port: 443,
+    //     chainId: 'aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906'
+    // }
         
     // static async connect() {
     //     let scatter = ScatterJS.scatter;
@@ -104,9 +110,57 @@ class ScatterLogin extends Component {
     //     }
     // }
 
+    async logintest(){
+        ScatterJS.scatter.connect('zjubca-bounty').then(connected => {
+            // If the user does not have Scatter or it is Locked or Closed this will return false;
+            this.connected = connected;
+            if(!connected) return false;
+            const scatter = ScatterJS.scatter;
+            // Now we need to get an identity from the user.
+            // We're also going to require an account that is connected to the network we're using.
+            const requiredFields = { accounts:[this.network] };
+            scatter.getIdentity(requiredFields).then(() => {
+                // Always use the accounts you got back from Scatter. Never hardcode them even if you are prompting
+                // the user for their account name beforehand. They could still give you a different account.
+                const account = scatter.identity.accounts.find(x => x.blockchain === 'eos');//
+                console.log("login success,", account);
+
+                // for(var i=0; i<scatter.identity.accounts.length; i++){
+                //     console.log("login success,", scatter.identity.accounts[i]);
+                //     // alert("login success" + JSON.stringify(this.currentAccount));
+                // }
+
+                // You can pass in any additional options you want into the eosjs reference.
+                const eosOptions = { expireInSeconds:60 };
+                // Get a proxy reference to eosjs which you can use to sign transactions with a user's Scatter.
+                const eos = scatter.eos(this.network, Eos, eosOptions);
+                // ----------------------------
+                // Now that we have an identity,
+                // an EOSIO account, and a reference
+                // to an eosjs object we can send a transaction.
+                // ----------------------------
+                // Never assume the account's permission/authority. Always take it from the returned account.
+                
+                // const transactionOptions = { authorization:[`${account.name}@${account.authority}`] };
+                // eos.transfer(account.name, 'helloworld', '1.0000 EOS', 'memo', transactionOptions).then(trx => {
+                //     // That's it!
+                //     console.log(`Transaction ID: ${trx.transaction_id}`);
+                // }).catch(error => {
+                //     console.error(error);
+                // });
+        
+            }).catch(error => {
+                // The user rejected this request, or doesn't have the appropriate requirements.
+                console.error("error : ",error);
+            });
+            console.log(this.connected);
+        });
+        
+    }
+
     async connect(){
         //change name 'hello-scatter' to your application's name
-        this.connected = await ScatterJS.scatter.connect('zjubca-bounty')//
+        this.connected = await ScatterJS.scatter.connect('zjubca-bounty')//zjubca-bounty
         console.log(this.connected);
     }
 
@@ -121,8 +175,10 @@ class ScatterLogin extends Component {
             let result = await ScatterJS.scatter.getIdentity({accounts:[this.network]})
             console.log("result: ",result);
             this.currentAccount = result.accounts[0];
-            console.log("login success,", this.currentAccount)
-            alert("login success" + JSON.stringify(this.currentAccount))
+            for(var i=0; i<result.accounts.length; i++){
+                console.log("login success,", this.currentAccount);
+                alert("login success" + JSON.stringify(this.currentAccount));
+            }
         } catch (e) {
             alert("login fail")
             console.log("login fail,", e)
@@ -181,6 +237,7 @@ class ScatterLogin extends Component {
     async handleLogin() {
         await this.connect()
         await this.login()
+        // await this.logintest();
     }
 
     handleChange(e) {
