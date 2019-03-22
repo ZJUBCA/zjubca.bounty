b776a9d0eadf9bb9d965dfc23d88881cd9c8c556
treasersimplifies <31153014+treasersimplifies@users.noreply.github.com>
2019-03-22 11:15:41 +0800

    modify frontend view component structure

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+
+
+class Footer extends Component {
+    constructor(props) {
+      super(props);
+    }
+    render(){
+        return (
+        <div id="footer_wrap" className="outer">
+            <footer className="inner">
+                <p className="copyright">
+                © 2019 ZJU BlockChain Association 浙江大学区块链协会<br/>
+                Other Links: <a href="https://github.com/Blockchain-zju"> ZJU BlockChain Association GitHub </a> |
+                <a href="https://toolkit.zjubca.org/"> ZJUBCA.EOS TOOLKIT </a> |
+                <a href="https://docs.zjubca.org"> ZJUBCA.DOCS </a> 
+                </p>
+            </footer>
+        </div>
+        )
+    }
+}
+
+export default Footer
---
+++ frontend/src/components/TaskFilter.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+
+class TaskFilter extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+        <div className="taskFilter">
+            <select>
+                <option value="tasktitle">任务名称</option>
+                <option value="taskstatus" selected="selected">任务状态</option>
+                <option value="taskauthor">任务创建人</option>
+                <option value="taskreward">任务奖励</option>
+            </select>
+            &nbsp; &nbsp; &nbsp; &nbsp;
+            <select>
+                <option value="equal" selected="selected">等于</option>
+                <option value="bigger">大于</option>
+                <option value="nosmaller">大于等于</option>
+                <option value="smaller">小于</option>
+                <option value="nobigger">小于等于</option>
+            </select>
+            &nbsp; &nbsp; &nbsp; &nbsp;
+            <input type="text"  name="filtervalue" id="filtervalue" 
+            placeholder="Before Executing"//作用是？？？
+            />
+            &nbsp; &nbsp; &nbsp; &nbsp;
+            <button>筛选</button>
+        </div>
+        );
+    }
+}
+
+export default TaskFilter;
