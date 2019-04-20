279d0aaf8139727b3c1fd3a14d8ab2a5efb7a355
treasersimplifies <31153014+treasersimplifies@users.noreply.github.com>
2019-04-20 23:16:00 +0800

    Updates

---
+++ precache-manifest.78ab221213d4e284116d77badea91be8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d8b29bfc9a66259c43b8",
+    "url": "/zjubca.bounty/static/js/runtime~main.ea615a8c.js"
+  },
+  {
+    "revision": "e9d03579a34a3cd27f35",
+    "url": "/zjubca.bounty/static/js/main.de989784.chunk.js"
+  },
+  {
+    "revision": "c40dd4d0dd20f9019291",
+    "url": "/zjubca.bounty/static/js/2.d564d5d0.chunk.js"
+  },
+  {
+    "revision": "e9d03579a34a3cd27f35",
+    "url": "/zjubca.bounty/static/css/main.467bf43b.chunk.css"
+  },
+  {
+    "revision": "0384ef106a1dd75e019e7bdf545bb18c",
+    "url": "/zjubca.bounty/index.html"
+  }
+];
