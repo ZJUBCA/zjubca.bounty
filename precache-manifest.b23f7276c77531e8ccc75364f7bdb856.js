8be92944d742b6a6a918cd878dd0e352cd818200
treasersimplifies <31153014+treasersimplifies@users.noreply.github.com>
2019-04-25 10:34:58 +0800

    Updates

--- precache-manifest.b23f7276c77531e8ccc75364f7bdb856.js
+++ precache-manifest.b23f7276c77531e8ccc75364f7bdb856.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/static/js/runtime~main.c5541365.js"
   },
   {
-    "revision": "e803fa42cb114af31654",
-    "url": "/static/js/main.ce62f665.chunk.js"
+    "revision": "bed1db87c67c7f0c7744",
+    "url": "/static/js/main.2d5ff556.chunk.js"
   },
   {
     "revision": "1038990375eef5a1fdab",
     "url": "/static/js/2.8f9dc979.chunk.js"
   },
   {
-    "revision": "915deeb0a71d27ccacd8fd5eb710a83f",
+    "revision": "1bc30aa83d819d1e97ce1c63593b14cf",
     "url": "/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d0ce2c133dd5b9ebf0777d7c27165d8b.js"
+  "/precache-manifest.b23f7276c77531e8ccc75364f7bdb856.js"
 );
 
 workbox.clientsClaim();
